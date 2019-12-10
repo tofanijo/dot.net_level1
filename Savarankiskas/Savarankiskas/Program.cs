f82bafaae656a6e8c8737e41d2c4ff5d058a5ad7
tofanijo <tofanijo@gmail.com>
2019-12-10 21:38:32 +0200

    spalva

---
+++ Savarankiskas/Savarankiskas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Savarankiskas
+{
+ class Program
+    {
+        static void Main(string[] args)
+        {
+            string spalva;
+            Console.WriteLine("iveskite savo spalva:");
+
+            spalva = Console.ReadLine();
+            Console.WriteLine("tu ivedei tokia spalva {0}", spalva);
+            Console.ReadLine();
+        }
+
+    }
+}
