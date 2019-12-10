844d4950c83083eb3083e7eb4ece2e13d0c09470
tofanijo <tofanijo@gmail.com>
2019-12-10 20:59:35 +0200

    pamoka3

---
+++ UserIO/UserIO/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vardas = "";
+            while (vardas != "stop")
+            {
+                Console.WriteLine("Iveskite savo varda:");
+                vardas = Console.ReadLine();
+                Console.WriteLine("Sveiki {0} [ENTER - testi]", vardas);
+            }
+            Console.Clear();
+            Console.WriteLine("Ate...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ pamoka 2/pamoka 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pamoka_2
+{
+    class Rectangle
+    {
+        private double length;
+        private double width;
+        public void Acceptedetails()
+        { length = 4.5;
+            width = 3.5;
+        }
+         public double GetArea()
+        { return length * width;
+        }
+        public void Display()
+        { Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+                }
+            }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.Acceptedetails();
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
