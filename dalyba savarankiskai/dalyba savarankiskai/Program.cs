dbf5d0affff8b972daa46274b38167bb9666d33b
tofanijo <tofanijo@gmail.com>
2019-12-11 08:11:53 +0200

    dalyba dalyba savarankiskai

---
+++ dalyba savarankiskai/dalyba savarankiskai/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dalyba_savarankiskai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double skaicius1 = 65;
+            double skaicius2 = 4;
+            Console.WriteLine("65 padalinta 4 = {0}", skaicius1 / skaicius2);
+            Console.ReadLine();
+
+        }
+    }
+}
