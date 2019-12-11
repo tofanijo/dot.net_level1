fbafc167081c0dd0900cc81acb2d541ead62383d
tofanijo <tofanijo@gmail.com>
2019-12-11 07:43:13 +0200

    dalyba 25/4

---
+++ dalyba/dalyba/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dalyba
+{ 
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double skaicius1 = 25;
+            double skaicius2 = 4;
+            Console.WriteLine("25 padalinta 4 = {0}", skaicius1 / skaicius2);
+            Console.ReadLine();
+        }
+    }
+}
