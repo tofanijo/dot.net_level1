3ae82cc8862a1c6f506b92212ce4e08d9f2e7f9f
tofanijo <tofanijo@gmail.com>
2019-12-11 10:48:58 +0200

    kokia spava 2019 12 11

---
+++ megstama spalva/megstama spalva/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace megstama_spalva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string melyna;
+            Console.WriteLine("kokia jusu megstamiausia spalva:");
+            melyna = Console.ReadLine();
+            Console.WriteLine("mano irgi {0}", melyna);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ sudetis ir dalyba/sudetis ir dalyba/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sudetis_ir_dalyba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double zirgas1 = 100;
+            double zirgas2 = 4;
+            double zirgas3 = 2;
+            Console.WriteLine("100 sudeta 4 {0}", zirgas1 + zirgas2);
+           
+            Console.ReadLine();
+        }
+    }
+}
