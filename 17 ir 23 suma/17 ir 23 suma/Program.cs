9c5c2eb29465d3e21138820eaa91c1358075b408
tofanijo <tofanijo@gmail.com>
2019-12-11 11:24:48 +0200

    17 ir 23 suma

---
+++ 17 ir 23 suma/17 ir 23 suma/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_ir_23_suma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double skaicius1 = 17;
+            double skaicius2 = 23;
+            Console.WriteLine("17 prideti 23 {0}", skaicius1 + skaicius2);
+            Console.ReadLine();
+        }
+    }
+}
