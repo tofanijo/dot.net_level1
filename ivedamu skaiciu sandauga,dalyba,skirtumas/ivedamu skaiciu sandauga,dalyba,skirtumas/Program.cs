5ddaf3bd8174dbba3df2764de7bf93ca9fa899d1
tofanijo <tofanijo@gmail.com>
2019-12-11 20:52:13 +0200

    balansas

---
+++ ivedamu skaiciu sandauga,dalyba,skirtumas/ivedamu skaiciu sandauga,dalyba,skirtumas/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ivedamu_skaiciu_sandauga_dalyba_skirtumas
+{
+    class Rectangle
+    {
+        private double skaicius3;
+        private double skaicius2;
+        public void Acceptedetails()
+        { skaicius3 = 3;
+            skaicius2 = 2;
+        }
+        public double GetArea()
+        {
+            return skaicius3 * skaicius2;
+        }
+        public void Display()
+        {
+            Console.WriteLine("skaicius1: {0}", skaicius3);
+            Console.WriteLine("skaicius2: {0}", skaicius2);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }
+        class Program
+        {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.Acceptedetails();
+            r.Display();
+            Console.ReadLine();
+
+            
+                }
+         
+        }
+    }
+
---
+++ sandauga dalyba/sandauga dalyba/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sandauga_dalyba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sveiki!");
+
+            string debit = "";
+            Console.WriteLine("skaicius1:");
+            debit = Console.ReadLine();
+            Console.WriteLine("skaicius1:" + debit);
+           
+            string credit = "";
+            Console.WriteLine("skaicius2:");
+            credit = Console.ReadLine();
+            Console.WriteLine("skaicius2:" + credit);
+            
+            int debitInt;
+            int creditInt = 0;
+            int.TryParse(debit, out debitInt);
+            int.TryParse(credit, out creditInt);
+            int balance = debitInt - creditInt;
+            Console.WriteLine("Skirtumas:" + balance.ToString());
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+    
+        
+        
+      
+
+
+            
+
+
+     
