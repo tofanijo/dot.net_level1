7178b23c1666d8687663ac8a303fa90787c1fb9c
tofanijo <tofanijo@gmail.com>
2019-12-11 08:24:09 +0200

    daugyba savarankiskai

---
+++ daugyba savarankiskai/daugyba savarankiskai/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace daugyba_savarankiskai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double skaicius1 = 2;
+            double skaicius2 = 5;
+            Console.WriteLine("5 padauginta 2 {0}", skaicius2 * skaicius1);
+            Console.ReadLine();
+        }
+    }
+}
