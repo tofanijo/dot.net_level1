16a2c1911e00286c0fd6213fda8204ad4b73e645
tofanijo <tofanijo@gmail.com>
2019-12-11 13:35:57 +0200

    rezultatu isvedimas

---
+++ rezultatu isvedimas/rezultatu isvedimas/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rezultatu_isvedimas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double skaicius1 = -6;
+            double skaicius2 = 3;
+            double skaicius3 = 5;
+            double skaicius4 = 13;
+            double skaicius5 = 2;
+            double skaicius6 = 7;
+            double skaicius7 = 5;
+            double skaicius8 = -2;
+            double skaicius9 = 20;
+            double skaicius10 = 10;
+            Console.WriteLine("-6 prideti 3 padauginti 5 {0}", skaicius1 + skaicius2 / skaicius3);
+             Console.WriteLine("13 atimti 2 padauginti 7 {0}", skaicius4 - skaicius5 * skaicius6);
+            Console.WriteLine("5 prideti -2 prideti 20 padalinti 10 {0}", skaicius7 + skaicius8 + skaicius9 / skaicius10);
+             Console.ReadLine();
+            
+
+                
+
+                
+
+
+          
+        }
+        
+        
+
+
+        }
+    }
+
+
+
+        
