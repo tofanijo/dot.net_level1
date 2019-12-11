3408fbd6e62b607548cc847758882f0b64fdcda8
tofanijo <tofanijo@gmail.com>
2019-12-11 12:32:10 +0200

    24  padalinta 5

---
+++ 24  padalinta 5/24  padalinta 5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24__padalinta_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double skaicius1 = 24;
+            double skaicius2 = 5;
+            Console.WriteLine("24 padalinta 5 {0}", skaicius1 / skaicius2);
+            Console.ReadLine();
+        }
+    }
+}
