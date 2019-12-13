c8461302a5ab2a60459589dacc16f88bcbc65e46
tofanijo <tofanijo@gmail.com>
2019-12-13 15:27:36 +0200

    Kvadratas

---
+++ Simboliu staciakampis/Simboliu staciakampis/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simboliu_staciakampis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string staciakampis = "";
+            Console.WriteLine("Iveskite simboli:");
+            staciakampis = Console.ReadLine();
+
+            Console.WriteLine("{0} {0} {0} {0} {0} {0}", staciakampis);
+            Console.WriteLine("{0} {0} {0} {0} {0} {0}", staciakampis);
+            Console.WriteLine("{0}         {0}", staciakampis);
+            Console.WriteLine("{0}         {0}", staciakampis);
+            Console.WriteLine("{0}         {0}", staciakampis);
+            Console.WriteLine("{0}         {0}", staciakampis);
+            Console.WriteLine("{0}         {0}", staciakampis);
+            Console.WriteLine("{0}         {0}", staciakampis);
+            Console.WriteLine("{0}         {0}", staciakampis);
+            Console.WriteLine("{0}         {0}", staciakampis);
+            Console.WriteLine("{0}         {0}", staciakampis);
+            Console.WriteLine("{0}         {0}", staciakampis);
+            Console.WriteLine("{0}         {0}", staciakampis);
+            Console.WriteLine("{0}         {0}", staciakampis);
+            Console.WriteLine("{0}         {0}", staciakampis);
+            Console.WriteLine("{0}         {0}", staciakampis);
+            Console.WriteLine("{0} {0} {0} {0} {0} {0}", staciakampis);
+            Console.WriteLine("{0} {0} {0} {0} {0} {0}", staciakampis);
+
+            Console.ReadLine();
+        }
+    }
+}
